44dffeeedcd36405b4b808ff9bfaaf7475c58191
Ethan Wells-Smith <69800526+ewsgit@users.noreply.github.com>
2021-01-12 11:23:21 +0000

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,17 +1,7 @@
 const Ampercache = "Ampersanv1"
 const Ampercachecomps = [
   "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/Forgot.html",
-  "/manifest.json",
-  "/favicon.ico",
-  "/navbars.css",
-  "/Apps.html",
-  "/settings_Account.html",
-  "/settings_User.html",
-  "/assets/Ampersan__alpha_small.png",
+  "/assets/",
 ]
 
 self.addEventListener("install", installEvent => {
@@ -26,5 +16,5 @@ self.addEventListener("fetch", fetchEvent => {
       caches.match(fetchEvent.request).then(res => {
         return res || fetch(fetchEvent.request)
       })
-    )
+    )   
   })
