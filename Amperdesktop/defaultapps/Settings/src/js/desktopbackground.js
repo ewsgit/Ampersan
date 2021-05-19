f28adeba55fb382e1223394f4e69dc244bd10881
X-Code-Software <ews2909@outlook.com>
2021-05-19 20:03:40 +0100

    wipe

--- Amperdesktop/defaultapps/Settings/src/js/desktopbackground.js
+++
@@ -1,3 +0,0 @@
-function desktopbackground1() {
-  document.getElementById("body").style.backgroundImage = url("/Ampersan/assets/Amperdesktop_Backgrounds/(3).PNG");
-};
--- Ampersan/js/version.js
+++
@@ -1,7 +0,0 @@
-var aboutampersan = [
- "v0.1.3",               //0
- "Development Release",  //1
- "Beta",                 //2
- "Ampersan Dev Team",    //3
- "Public Release"        //4
-];
